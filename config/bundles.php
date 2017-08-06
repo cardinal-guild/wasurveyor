6af79a1ab8c188b6ae9b2f61727dd9809e5b2331
Matt Brunt <matt@mfyu.co.uk>
2017-08-06 20:33:08 +0100

    Strip down flex

--- config/bundles.php
+++ config/bundles.php
@@ -2,12 +2,7 @@
 
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
-    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
-    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
-    'Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle' => ['all' => true],
-    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
-    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
     'Symfony\Bundle\DebugBundle\DebugBundle' => ['dev' => true, 'test' => true],
     'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,25 +1,15 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 Class HomeController extends Controller
 {
-    public function welcome(Request $request)
+    public function welcome(): Response
     {
-        $session = $request->getSession();
-        $number = $session->get('random_number', null);
-
-        if ($number === null) {
-            $number = random_int(0, PHP_INT_MAX);
-            $session->set('random_number', $number);
-        }
-
-        return $this->render('home/welcome.html.twig', [
-            'hostname'      => $_SERVER['HOSTNAME'],
-            'random_number' => $number
-        ]);
+        return new Response('Hello, world!');
     }
 }
