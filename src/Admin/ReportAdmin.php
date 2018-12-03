6bee847278b788f80be2ed5411c84d67b16b8a3b
Jan Ooms <flexyjean@gmail.com>
2018-12-03 10:54:50 +0100

    Report approving implemented

--- src/Admin/ReportAdmin.php
+++ src/Admin/ReportAdmin.php
@@ -9,11 +9,28 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelAutocompleteType;
 use Sonata\AdminBundle\Form\Type\ModelListType;
 use Sonata\AdminBundle\Form\Type\ModelType;
+use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class ReportAdmin extends AbstractAdmin
 {
+
+    protected $baseRouteName = 'cg_report_admin';
+    protected $baseRoutePattern = 'report';
+
+    protected $datagridValues = array(
+
+        // display the first page (default = 1)
+        '_page' => 1,
+
+        // reverse order (default = 'ASC')
+        '_sort_order' => 'DESC',
+
+        // name of the ordered field (default = the model's id field, if any)
+        '_sort_by' => 'id',
+    );
+
     /**
      * @param DatagridMapper $datagridMapper
      */
@@ -22,17 +39,21 @@ class ReportAdmin extends AbstractAdmin
         $datagridMapper
             ->add('id')
             ->add('island')
-            ->add('databanks')
-            ->add('revivalChambers')
-            ->add('turrets')
-            ->add('dangerous')
             ->add('metals')
-            ->add('trees')
             ->add('createdAt')
             ->add('updatedAt')
         ;
     }
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('create');
+        $collection->remove('edit');
+        $collection->remove('delete');
 
+        $collection->add('approve', $this->getRouterIdParameter().'/approve');
+        $collection->add('approveOverride', $this->getRouterIdParameter().'/approve-override');
+        $collection->add('disapprove', $this->getRouterIdParameter().'/disapprove');
+    }
     /**
      * @param ListMapper $listMapper
      */
@@ -40,14 +61,17 @@ class ReportAdmin extends AbstractAdmin
     {
         $listMapper
             ->add('id')
-            ->addIdentifier('type')
+            ->add('island')
+            ->add('metals', null, ['label'=>'Reported metals', 'template' => 'admin/list_reported_metals.html.twig'])
+            ->add('existingMetals', null, ['label'=>'Current metals', 'template' => 'admin/list_existing_metals.html.twig'])
+            ->add('mode', null, ['label'=>'Map Mode', 'template' => 'admin/list_report_mode.html.twig'])
+            ->add('island.tier')
             ->add('createdAt')
-            ->add('updatedAt')
             ->add('_action', null, array(
                 'actions' => array(
-                    'show' => array(),
-                    'edit' => array(),
-                    'delete' => array(),
+                    'approve' => ['template'=>'admin/list_report_approve_action.html.twig'],
+                    'approveOverride' => ['template'=>'admin/list_report_approve_override_action.html.twig'],
+                    'disapprove' => ['template'=>'admin/list_report_disapprove_action.html.twig'],
                 )
             ))
         ;
@@ -59,7 +83,7 @@ class ReportAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('type', ModelType::class, ["btn_add" => false])
+            ->add('metals')
         ;
     }
 
@@ -70,7 +94,8 @@ class ReportAdmin extends AbstractAdmin
     {
         $showMapper
             ->add('id')
-            ->add('type')
+            ->add('island')
+            ->add('metals')
             ->add('createdAt')
             ->add('updatedAt')
         ;
---
+++ src/Controller/Admin/ReportAdminController.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\IslandPVEMetal;
+use App\Entity\IslandPVPMetal;
+use App\Entity\Report;
+use App\Entity\ReportMetal;
+use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ReportAdminController extends CRUDController
+{
+
+    /**
+     * @param int|string|null $id
+     *
+     * @return Response
+     *
+     * @throws NotFoundHttpException If the object does not exist
+     * @throws AccessDeniedException If access is not granted
+     */
+    public function approveAction($id = null)
+    {
+        if ($this->hasAccess('approve')) {
+            $request = $this->getRequest();
+            $em = $this->getDoctrine()->getManager();
+            $id = $request->get($this->admin->getIdParameter());
+            /**
+             * @var $report Report
+             */
+            $report = $this->admin->getObject($id);
+
+            if ($report && $report->getIsland()) {
+                $island = $report->getIsland();
+
+                if ($report->getMode() === Report::PVE) {
+                    $islandMetals = $island->getPveMetals();
+                } else {
+                    $islandMetals = $island->getPvpMetals();
+                }
+
+                $metalsToAdd = [];
+                foreach ($report->getMetals() as $reportMetal) {
+                    $exists = false;
+                    foreach ($islandMetals as $islandMetal) {
+                        if ($reportMetal->getType() === $islandMetal->getType()) {
+                            $exists = true;
+                            break;
+                        }
+                    }
+                    if (!$exists) {
+                        $metalsToAdd[] = $reportMetal;
+                    }
+                }
+                if (!count($metalsToAdd)) {
+                    $this->addFlash(
+                        'sonata_flash_error',
+                        'Report not processed, no new metals to add.  Please use override or disapprove this report'
+                    );
+
+
+                } else {
+                    /**
+                     * @var $metal ReportMetal
+                     */
+                    foreach ($metalsToAdd as $metal) {
+                        if ($report->getMode() === Report::PVE) {
+                            $islandMetal = new IslandPVEMetal();
+                            $islandMetal->setType($metal->getType());
+                            $islandMetal->setQuality($metal->getQuality());
+                            $island->addPveMetal($islandMetal);
+                        } else {
+                            $islandMetal = new IslandPVPMetal();
+                            $islandMetal->setType($metal->getType());
+                            $islandMetal->setQuality($metal->getQuality());
+                            $island->addPvpMetal($islandMetal);
+                        }
+                    }
+                    $em->persist($island);
+                    $em->remove($report);
+                    $em->flush();
+
+
+                    $this->addFlash(
+                        'sonata_flash_success',
+                        'Report approved, metals added to island without override'
+                    );
+                }
+            }
+
+            return $this->redirectToList();
+        }
+    }
+
+    /**
+     * @param int|string|null $id
+     *
+     * @return Response
+     *
+     * @throws NotFoundHttpException If the object does not exist
+     * @throws AccessDeniedException If access is not granted
+     */
+    public function approveOverrideAction($id = null)
+    {
+        if ($this->hasAccess('approveOverride')) {
+            $request = $this->getRequest();
+            $em = $this->getDoctrine()->getManager();
+            $id = $request->get($this->admin->getIdParameter());
+            /**
+             * @var $report Report
+             */
+            $report = $this->admin->getObject($id);
+
+            if ($report && $report->getIsland()) {
+                $island = $report->getIsland();
+
+                if ($report->getMode() === Report::PVE) {
+                    $islandMetals = $island->getPveMetals();
+                } else {
+                    $islandMetals = $island->getPvpMetals();
+                }
+
+                $metalsToAdd = [];
+                foreach ($report->getMetals() as $reportMetal) {
+                    $exists = false;
+                    foreach ($islandMetals as $islandMetal) {
+                        if ($reportMetal->getType() === $islandMetal->getType()) {
+                            $islandMetal->setQuality($reportMetal->getQuality());
+                            $exists = true;
+                        }
+                    }
+                    if (!$exists) {
+                        if ($report->getMode() === Report::PVE) {
+                            $islandMetal = new IslandPVEMetal();
+                            $islandMetal->setType($reportMetal->getType());
+                            $islandMetal->setQuality($reportMetal->getQuality());
+                            $island->addPveMetal($islandMetal);
+                        } else {
+                            $islandMetal = new IslandPVPMetal();
+                            $islandMetal->setType($reportMetal->getType());
+                            $islandMetal->setQuality($reportMetal->getQuality());
+                            $island->addPvpMetal($islandMetal);
+                        }
+                    }
+                }
+                $em->persist($island);
+                $em->remove($report);
+                $em->flush();
+                $this->addFlash(
+                    'sonata_flash_success',
+                    'Report approved with override, all metals overridden with reported metals and qualities'
+                );
+            }
+            return $this->redirectToList();
+        }
+    }
+
+    /**
+     * @param int|string|null $id
+     *
+     * @return Response
+     *
+     * @throws NotFoundHttpException If the object does not exist
+     * @throws AccessDeniedException If access is not granted
+     */
+    public function disapproveAction($id = null)
+    {
+
+        if ($this->hasAccess('disapprove')) {
+            $request = $this->getRequest();
+            $em = $this->getDoctrine()->getManager();
+            $id = $request->get($this->admin->getIdParameter());
+            $report = $this->admin->getObject($id);
+
+            $em->remove($report);
+            $em->flush();
+
+
+            $this->addFlash(
+                'sonata_flash_success',
+                'Report disapproved, removed from database'
+            );
+            return $this->redirectToList();
+        }
+    }
+}
