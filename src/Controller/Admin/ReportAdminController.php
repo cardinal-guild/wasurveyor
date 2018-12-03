34303844b3925d837758da518e1bee34ff740520
Jan Ooms <flexyjean@gmail.com>
2018-12-03 11:01:24 +0100

    Fix for reporting access control

--- src/Controller/Admin/ReportAdminController.php
+++ src/Controller/Admin/ReportAdminController.php
@@ -24,75 +24,73 @@ class ReportAdminController extends CRUDController
      */
     public function approveAction($id = null)
     {
-        if ($this->hasAccess('approve')) {
-            $request = $this->getRequest();
-            $em = $this->getDoctrine()->getManager();
-            $id = $request->get($this->admin->getIdParameter());
-            /**
-             * @var $report Report
-             */
-            $report = $this->admin->getObject($id);
+        $request = $this->getRequest();
+        $em = $this->getDoctrine()->getManager();
+        $id = $request->get($this->admin->getIdParameter());
+        /**
+         * @var $report Report
+         */
+        $report = $this->admin->getObject($id);
 
-            if ($report && $report->getIsland()) {
-                $island = $report->getIsland();
+        if ($report && $report->getIsland()) {
+            $island = $report->getIsland();
 
-                if ($report->getMode() === Report::PVE) {
-                    $islandMetals = $island->getPveMetals();
-                } else {
-                    $islandMetals = $island->getPvpMetals();
-                }
-
-                $metalsToAdd = [];
-                foreach ($report->getMetals() as $reportMetal) {
-                    $exists = false;
-                    foreach ($islandMetals as $islandMetal) {
-                        if ($reportMetal->getType() === $islandMetal->getType()) {
-                            $exists = true;
-                            break;
-                        }
-                    }
-                    if (!$exists) {
-                        $metalsToAdd[] = $reportMetal;
-                    }
-                }
-                if (!count($metalsToAdd)) {
-                    $this->addFlash(
-                        'sonata_flash_error',
-                        'Report not processed, no new metals to add.  Please use override or disapprove this report'
-                    );
-
-
-                } else {
-                    /**
-                     * @var $metal ReportMetal
-                     */
-                    foreach ($metalsToAdd as $metal) {
-                        if ($report->getMode() === Report::PVE) {
-                            $islandMetal = new IslandPVEMetal();
-                            $islandMetal->setType($metal->getType());
-                            $islandMetal->setQuality($metal->getQuality());
-                            $island->addPveMetal($islandMetal);
-                        } else {
-                            $islandMetal = new IslandPVPMetal();
-                            $islandMetal->setType($metal->getType());
-                            $islandMetal->setQuality($metal->getQuality());
-                            $island->addPvpMetal($islandMetal);
-                        }
-                    }
-                    $em->persist($island);
-                    $em->remove($report);
-                    $em->flush();
-
-
-                    $this->addFlash(
-                        'sonata_flash_success',
-                        'Report approved, metals added to island without override'
-                    );
-                }
+            if ($report->getMode() === Report::PVE) {
+                $islandMetals = $island->getPveMetals();
+            } else {
+                $islandMetals = $island->getPvpMetals();
             }
 
-            return $this->redirectToList();
+            $metalsToAdd = [];
+            foreach ($report->getMetals() as $reportMetal) {
+                $exists = false;
+                foreach ($islandMetals as $islandMetal) {
+                    if ($reportMetal->getType() === $islandMetal->getType()) {
+                        $exists = true;
+                        break;
+                    }
+                }
+                if (!$exists) {
+                    $metalsToAdd[] = $reportMetal;
+                }
+            }
+            if (!count($metalsToAdd)) {
+                $this->addFlash(
+                    'sonata_flash_error',
+                    'Report not processed, no new metals to add.  Please use override or disapprove this report'
+                );
+
+
+            } else {
+                /**
+                 * @var $metal ReportMetal
+                 */
+                foreach ($metalsToAdd as $metal) {
+                    if ($report->getMode() === Report::PVE) {
+                        $islandMetal = new IslandPVEMetal();
+                        $islandMetal->setType($metal->getType());
+                        $islandMetal->setQuality($metal->getQuality());
+                        $island->addPveMetal($islandMetal);
+                    } else {
+                        $islandMetal = new IslandPVPMetal();
+                        $islandMetal->setType($metal->getType());
+                        $islandMetal->setQuality($metal->getQuality());
+                        $island->addPvpMetal($islandMetal);
+                    }
+                }
+                $em->persist($island);
+                $em->remove($report);
+                $em->flush();
+
+
+                $this->addFlash(
+                    'sonata_flash_success',
+                    'Report approved, metals added to island without override'
+                );
+            }
         }
+
+        return $this->redirectToList();
     }
 
     /**
@@ -105,57 +103,55 @@ class ReportAdminController extends CRUDController
      */
     public function approveOverrideAction($id = null)
     {
-        if ($this->hasAccess('approveOverride')) {
-            $request = $this->getRequest();
-            $em = $this->getDoctrine()->getManager();
-            $id = $request->get($this->admin->getIdParameter());
-            /**
-             * @var $report Report
-             */
-            $report = $this->admin->getObject($id);
+        $request = $this->getRequest();
+        $em = $this->getDoctrine()->getManager();
+        $id = $request->get($this->admin->getIdParameter());
+        /**
+         * @var $report Report
+         */
+        $report = $this->admin->getObject($id);
 
-            if ($report && $report->getIsland()) {
-                $island = $report->getIsland();
+        if ($report && $report->getIsland()) {
+            $island = $report->getIsland();
 
-                if ($report->getMode() === Report::PVE) {
-                    $islandMetals = $island->getPveMetals();
-                } else {
-                    $islandMetals = $island->getPvpMetals();
-                }
-
-                $metalsToAdd = [];
-                foreach ($report->getMetals() as $reportMetal) {
-                    $exists = false;
-                    foreach ($islandMetals as $islandMetal) {
-                        if ($reportMetal->getType() === $islandMetal->getType()) {
-                            $islandMetal->setQuality($reportMetal->getQuality());
-                            $exists = true;
-                        }
-                    }
-                    if (!$exists) {
-                        if ($report->getMode() === Report::PVE) {
-                            $islandMetal = new IslandPVEMetal();
-                            $islandMetal->setType($reportMetal->getType());
-                            $islandMetal->setQuality($reportMetal->getQuality());
-                            $island->addPveMetal($islandMetal);
-                        } else {
-                            $islandMetal = new IslandPVPMetal();
-                            $islandMetal->setType($reportMetal->getType());
-                            $islandMetal->setQuality($reportMetal->getQuality());
-                            $island->addPvpMetal($islandMetal);
-                        }
-                    }
-                }
-                $em->persist($island);
-                $em->remove($report);
-                $em->flush();
-                $this->addFlash(
-                    'sonata_flash_success',
-                    'Report approved with override, all metals overridden with reported metals and qualities'
-                );
+            if ($report->getMode() === Report::PVE) {
+                $islandMetals = $island->getPveMetals();
+            } else {
+                $islandMetals = $island->getPvpMetals();
             }
-            return $this->redirectToList();
+
+            $metalsToAdd = [];
+            foreach ($report->getMetals() as $reportMetal) {
+                $exists = false;
+                foreach ($islandMetals as $islandMetal) {
+                    if ($reportMetal->getType() === $islandMetal->getType()) {
+                        $islandMetal->setQuality($reportMetal->getQuality());
+                        $exists = true;
+                    }
+                }
+                if (!$exists) {
+                    if ($report->getMode() === Report::PVE) {
+                        $islandMetal = new IslandPVEMetal();
+                        $islandMetal->setType($reportMetal->getType());
+                        $islandMetal->setQuality($reportMetal->getQuality());
+                        $island->addPveMetal($islandMetal);
+                    } else {
+                        $islandMetal = new IslandPVPMetal();
+                        $islandMetal->setType($reportMetal->getType());
+                        $islandMetal->setQuality($reportMetal->getQuality());
+                        $island->addPvpMetal($islandMetal);
+                    }
+                }
+            }
+            $em->persist($island);
+            $em->remove($report);
+            $em->flush();
+            $this->addFlash(
+                'sonata_flash_success',
+                'Report approved with override, all metals overridden with reported metals and qualities'
+            );
         }
+        return $this->redirectToList();
     }
 
     /**
@@ -169,21 +165,19 @@ class ReportAdminController extends CRUDController
     public function disapproveAction($id = null)
     {
 
-        if ($this->hasAccess('disapprove')) {
-            $request = $this->getRequest();
-            $em = $this->getDoctrine()->getManager();
-            $id = $request->get($this->admin->getIdParameter());
-            $report = $this->admin->getObject($id);
+        $request = $this->getRequest();
+        $em = $this->getDoctrine()->getManager();
+        $id = $request->get($this->admin->getIdParameter());
+        $report = $this->admin->getObject($id);
 
-            $em->remove($report);
-            $em->flush();
+        $em->remove($report);
+        $em->flush();
 
 
-            $this->addFlash(
-                'sonata_flash_success',
-                'Report disapproved, removed from database'
-            );
-            return $this->redirectToList();
-        }
+        $this->addFlash(
+            'sonata_flash_success',
+            'Report disapproved, removed from database'
+        );
+        return $this->redirectToList();
     }
 }
